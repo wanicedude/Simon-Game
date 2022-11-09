48e31e369fb828bc5eee5a8b6325df9ba086772b
Wanice <52546308+wanicedude@users.noreply.github.com>
2022-11-09 14:15:06 +0100

    Update game.js
    Step 2: Create a new Pattern

--- game.js
+++ game.js
@@ -1,6 +1,23 @@
+// ==================== Step 2: Create a new pattern    ===========================
+
+// Create an empty array
+gamePattern = []
+
+
+// Create a list of colours
 buttonColours = ["red", "blue", "green", "yellow"]
 
+
+// Create a function that generates random number between 0 and 3
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4) ;
-    console.log(randomNumber)
+    return randomNumber;
 }
+
+
+// assign random colour
+randomChosenColour = buttonColours[nextSequence()]
+
+// push random color to gamePattern
+gamePattern.push(randomChosenColour)
+
